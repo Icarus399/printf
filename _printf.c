694f73120acbacaad88acc6f325ae4d2aa77a879
Icarus399 <3530@holbertonschool.com>
2022-03-15 14:33:58 +0000

    adding file

--- _printf.c
+++ _printf.c
@@ -1,25 +1,59 @@
 #include "main.h"
-#include <stdarg.h>
-
-/**
- * _printf - this function produces output according to a format.
- * Return: The number of characters printed.
- * @format: charater string. Composed of zero or more directives.
- */
-
-int _printf(const char *format, ...)
+int _printf(char *format, ...)
 {
-	int i, lenght;
+	char *p;
+	int idx;
+	char *str;
+	int len;
 
-	va_list (args);
+	va_list args;
+	va_start (args, format);
 
-	if (format == '\0')
-		return (-1);
+	p = format;
 
-	va_start(args, format);
+	for (p = format; *p != '\0'; p++)
+	{
+		if (*p != '%')
+		{
+			len += _putchar(*p);
+			continue;
+		}
+		p++;
 
-	if (args == '\0')
-		return (-1);
+		switch (*p)
+		{
+			case 'c': idx = va_arg(args, int);
+					  len += _putchar(idx);
+					  break;
 
-	
+			case 'd': idx = va_arg(args, int);
+					  if (idx < 0)
+					  {
+						  idx = -idx;
+						  len += _putchar('-');
+					  }
+					  _puts(convert(idx, 10));
+					  break;
 
+			case 'i': idx = va_arg(args, int);
+					  if (idx < 0)
+					  {
+						  idx = -idx;
+						  len += _putchar('-');
+					  }
+					  _puts(convert(idx, 10));
+					  break;
+
+			case 's': str = va_arg(args,char *);
+					  len += _puts(str);
+					  break;
+			default:
+
+					  _putchar('%');
+					  _putchar(*p);
+					  break;
+		}
+	}
+	va_end(args);
+	return (len);
+}
