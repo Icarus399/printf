977339420a0f736d41d3b81ddd33a188ec948dc0
Icarus399 <3530@holbertonschool.com>
2022-03-15 14:36:10 +0000

    adding header

--- main.h
+++ main.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
 
 int _printf(const char *format, ...);
 
+int printnum(int idx, int count);
 
+int _puts(const char *s);
+
+int _putchar(char c);
+#endif /* MAIN_H */
