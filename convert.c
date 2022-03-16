4fb0b3bdceb2fff693d953d8b888cd092dcdfe4e
Yaniel <yanielddiaz@gmail.com>
2022-03-16 01:20:06 +0000

    betty changes

--- convert.c
+++ convert.c
@@ -1,18 +1,25 @@
 #include "main.h"
 
+/**
+  * convert - converts one type of data into another
+  * @num: number
+  * @base: ...
+  * Return: characters
+  */
+
 char *convert(unsigned int num, int base)
 {
-	static char character[]= "0123456789ABCDEF";
+	static const char character[] = "0123456789ABCDEF";
 	static char buffer[50];
 	char *ptr;
 
 	ptr = &buffer[49];
 	*ptr = '\0';
 
-	while(num != 0)
+	while (num != 0)
 	{
 		*--ptr = character[num % base];
 		num = num / base;
 	}
-	return(ptr);
+	return (ptr);
 }
